6b10c0fff7c92afaa59b0040f6b0182978f3d67a
Jahred Hope <jhope@seek.com.au>
2017-12-04 21:38:53 +1100

    fix tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,15 @@
 module.exports = {
-  extends: 'react-app',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 7,
+    jsx: true
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
   rules: {
     'jsx-a11y/href-no-hash': 'off'
   }
